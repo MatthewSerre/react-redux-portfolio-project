70ba4b46fd0536fe2696ad6d4ce68ef2b1cd5423
MatthewSerre <matthew.serre@gmail.com>
2019-10-10 14:36:36 -0700

    Install react-bootstrap and include CDN stylesheet links in index.html to test imported components in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
 function App() {
   return (
@@ -18,7 +20,22 @@ function App() {
         >
           Learn React
         </a>
+        <ButtonToolbar>
+  <Button variant="primary">Primary</Button>
+  <Button variant="secondary">Secondary</Button>
+  <Button variant="success">Success</Button>
+  <Button variant="warning">Warning</Button>
+  <Button variant="danger">Danger</Button>
+  <Button variant="info">Info</Button>
+  <Button variant="light">Light</Button>
+  <Button variant="dark">Dark</Button>
+  <Button variant="link">Link</Button>
+</ButtonToolbar>
       </header>
+      <div className="container">
+        <h1>My First Bootstrap Page</h1>
+        <p>This is some text.</p>
+      </div>
     </div>
   );
 }
