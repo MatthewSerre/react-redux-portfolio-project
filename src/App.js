ea69adf5d55b4249a7da5ee4a0e49027eed57563
MatthewSerre <matthew.serre@gmail.com>
2019-10-10 17:02:11 -0700

    Install redux and create store to pass in with Provider wrapper around App component in index.js

--- src/App.js
+++ src/App.js
@@ -20,17 +20,6 @@ function App() {
         >
           Learn React
         </a>
-        <ButtonToolbar>
-  <Button variant="primary">Primary</Button>
-  <Button variant="secondary">Secondary</Button>
-  <Button variant="success">Success</Button>
-  <Button variant="warning">Warning</Button>
-  <Button variant="danger">Danger</Button>
-  <Button variant="info">Info</Button>
-  <Button variant="light">Light</Button>
-  <Button variant="dark">Dark</Button>
-  <Button variant="link">Link</Button>
-</ButtonToolbar>
       </header>
       <div className="container">
         <h1>My First Bootstrap Page</h1>
---
+++ src/reducers/manageAmiibo.js
@@ -0,0 +1,5 @@
+export default function manageAmiibo(state = {
+    amiibos: [],
+}, action) {
+    return state;
+    }
